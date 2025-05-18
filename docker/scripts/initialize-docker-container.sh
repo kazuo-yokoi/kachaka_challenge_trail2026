0b1986beb0b8b1ae3657cd6cda930dda384d86d3
uamea <uamea1987@gmail.com>
2025-05-18 20:40:40 +0900

    feature: automatic uv setup

--- docker/scripts/initialize-docker-container.sh
+++ docker/scripts/initialize-docker-container.sh
@@ -2,6 +2,13 @@
 
 alias create_ros2_pkg="cookiecutter ../../template/"
 
+if [ ! -d "/app/.venv" ]; then
+    echo "Creating virtual environment..."
+    cd /app
+    uv venv
+    uv sync
+fi
+
 # Keep the Docker container running in the background.
 # https://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
 tail -f /dev/null
