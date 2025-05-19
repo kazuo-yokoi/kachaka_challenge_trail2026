461ab1752bfd80b28a20e6cd62168767be1af8f8
uamea <uamea1987@gmail.com>
2025-05-19 14:16:15 +0900

    feature: added speak service and trail_kachaka_msg

--- ros2_ws/src/kachaka_utils/src/kachaka_utils/speak.py
+++ ros2_ws/src/kachaka_utils/src/kachaka_utils/speak.py
@@ -3,31 +3,55 @@ from kachaka_interfaces.action import ExecKachakaCommand
 from kachaka_interfaces.msg import KachakaCommand
 from rclpy.action import ActionClient
 from rclpy.node import Node
+from trail_kachaka_msgs.srv import KachakaSpeak
 
 
 class Speak(Node):
     def __init__(self) -> None:
-        super().__init__("speak")
+        super().__init__("kachaka_speak_node")
         self._action_client = ActionClient(
             self, ExecKachakaCommand, "/kachaka/kachaka_command/execute"
         )
         self._action_client.wait_for_server()
 
-    def send_goal(self, text: str):
-        command = KachakaCommand()
-        command.command_type = KachakaCommand.SPEAK_COMMAND
-        command.speak_command_text = text 
-        goal_msg = ExecKachakaCommand.Goal()
-        goal_msg.kachaka_command = command
-        return self._action_client.send_goal_async(goal_msg)
+        self.speak_srv = self.create_service(
+            KachakaSpeak, "/kachaka/speak", self.speak_callback
+        )
+        self.get_logger().info("Speak service is ready.")
+
+    def speak_callback(self, request, response):
+        self.get_logger().info(
+            f"Received request: text: {request.text}, wait: {request.wait}"
+        )
+        try:
+            command = KachakaCommand()
+            command.command_type = KachakaCommand.SPEAK_COMMAND
+            command.speak_command_text = request.text
+            goal_msg = ExecKachakaCommand.Goal()
+            goal_msg.kachaka_command = command
+            if request.wait:
+                self._action_client.send_goal_async(goal_msg)
+            else:
+                self._action_client.send_goal(goal_msg)
+        except Exception as e:
+            self.get_logger().error(f"Failed to send goal: {e}")
+            response.success = False
+            return response
+        response.success = True
+        return response
 
 
 def main(args=None):
     rclpy.init(args=args)
+    speak_node = Speak()
+    try:
+        rclpy.spin(speak_node)
+    except KeyboardInterrupt:
+        speak_node.get_logger().info("Keyboard interrupt, shutting down...")
+    finally:
+        speak_node.destroy_node()
+        rclpy.shutdown()
 
-    speak = Speak()
-    future = speak.send_goal("Please speak this ")
-    rclpy.spin_until_future_complete(speak, future)
 
-    speak.destroy_node()
-    rclpy.shutdown()
+if __name__ == "__main__":
+    main()
